3cdd761854c1dbce2bf967284a4da248f17b6162
dariopalmesi <dariopalmesi88@gmail.com>
2025-02-11 14:50:01 +0100

    add snack 3

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const quadrato = num => num * num
+const sottrai = (num1, num2) => num1 - num2
 
-console.log(quadrato(5));
+function dividi(num1, num2) {
+    return (num1 / num2).toFixed(2)
+}
 
+const eseguiOperazine = (num1, num2, risultato) => risultato(num1, num2)
+
+console.log(eseguiOperazine(3, 4, sottrai));
+console.log(eseguiOperazine(145, 65, dividi));
 
