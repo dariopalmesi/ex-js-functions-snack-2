9725b73d1c3feebe93712144723d07c32bd44cee
dariopalmesi <dariopalmesi88@gmail.com>
2025-02-11 15:00:05 +0100

    add snack 4

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-const sottrai = (num1, num2) => num1 - num2
-
-function dividi(num1, num2) {
-    return (num1 / num2).toFixed(2)
+function creaTimer() {
+    setTimeout(() => {
+        console.log('Tempo Scaduto');
+    }, 1000)
 }
 
-const eseguiOperazine = (num1, num2, risultato) => risultato(num1, num2)
-
-console.log(eseguiOperazine(8, 4, sottrai));
-console.log(eseguiOperazine(145, 65, dividi));
-
+creaTimer()
